7d63804500dd69f17b2e449a5e61c09de746b763
Ali <ali.hamid9119@gmail.com>
2024-02-29 00:23:36 +0100

    displaying slider value in text input

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
 let copyText = document.getElementById("input");
-
-function Submit() {
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-
-    let tooltip = document.getElementById("tooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
-}
-
-function outFunc() {
-    let tooltip = document.getElementById("tooltip");
-    tooltip.innerHTML = "Copy to clipboard";
-}
+let tooltip = document.getElementById("tooltip");
 
 let image = document.getElementById("img");
 
 let output = document.getElementsByClassName("slidercontainer");
-let sliderTopLeft = document.getElementById("topLeft");
-let sliderTopRight = document.getElementById("topRight");
-let sliderBottomLeft = document.getElementById("bottomLeft");
-let sliderBottomRight = document.getElementById("bottomRight");
+let sliderTopLeft = document.getElementById("topLeftX");
+let sliderTopRight = document.getElementById("topRightX");
+let sliderBottomLeft = document.getElementById("bottomLeftX");
+let sliderBottomRight = document.getElementById("bottomRightX");
 
 output.innerHTML = sliderTopLeft.value;
 output.innerHTML = sliderTopRight.value;
 output.innerHTML = sliderBottomLeft.value;
 output.innerHTML = sliderBottomRight.value;
 
+function borderRadiusUpdate() {
+  copyText.value = sliderTopLeft.value + "%" + " " +
+                   sliderTopRight.value + "%" + " " +
+                   sliderBottomRight.value + "%" + " " +
+                   sliderBottomLeft.value + "%";
+}
+
 sliderTopLeft.oninput = function() {
   image.style.borderTopLeftRadius = sliderTopLeft.value + "%";
+  borderRadiusUpdate();
 }
 
 sliderTopRight.oninput = function() {
   image.style.borderTopRightRadius = sliderTopRight.value + "%";
-}
-
-sliderBottomLeft.oninput = function() {
-  image.style.borderBottomLeftRadius = sliderBottomLeft.value + "%";
+  borderRadiusUpdate();
 }
 
 sliderBottomRight.oninput = function() {
   image.style.borderBottomRightRadius = sliderBottomRight.value + "%";
+  borderRadiusUpdate();
+}
+
+sliderBottomLeft.oninput = function() {
+  image.style.borderBottomLeftRadius = sliderBottomLeft.value + "%";
+  borderRadiusUpdate();
+}
+
+function Submit() {
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  tooltip.innerHTML = "Copy to clipboard";
 }
